a426f8e1a2490e09bca906ae0a9cab0c27e24acb
GiovannyGarzon <giovankyandres@gmail.com>
2023-06-22 04:39:37 -0400

    github actions

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import {connect} from 'mongoose'
 
 export const connectDatabase = async() => {
     try{
-        await connect(process.env.DATABASE) //mongodb+srv://${process.env.DATABASE}/manga-app?retryWrites=true&w=majority
+        await connect(`mongodb+srv://${process.env.ATLAS_URI}/mail-auth?retryWrites=true&w=majority`) //mongodb+srv://${process.env.DATABASE}/mail-auth?retryWrites=true&w=majority
         console.log('Database is connected')
     }catch(err){
         console.log(err)
