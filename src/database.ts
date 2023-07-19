07f61bbc87871610c7467215a961a71f0d3c9851
Giovanky <giovankyandres@gmail.com>
2023-07-18 23:08:59 -0400

    database uri changed

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import {DATABASE} from './config'
 export const connectDatabase = async() => {
     try{
         //mongodb+srv://${DATABASE}/mail-auth?retryWrites=true&w=majority
-        await connect(`mongodb://localhost:27017/mail-auth`) 
+        await connect(`mongodb+srv://${DATABASE}/mail-auth?retryWrites=true&w=majority`) 
         console.log('Database is connected')
     }catch(err){
         console.log(err)
